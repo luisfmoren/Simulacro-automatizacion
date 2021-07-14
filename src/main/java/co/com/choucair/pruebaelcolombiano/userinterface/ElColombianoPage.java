ad047c715be416c669b058d80a28f0c0ecc2ee0a
luifemor <luifemor@bancolombia.com.co>
2021-07-14 08:49:13 -0500

    se agrega el proyecto

--- src/main/java/co/com/choucair/pruebaelcolombiano/userinterface/ElColombianoPage.java
+++
@@ -1,8 +0,0 @@
-package co.com.choucair.pruebaelcolombiano.userinterface;
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("https://www.elcolombiano.com/")
-public class ElColombianoPage extends PageObject {
-}
